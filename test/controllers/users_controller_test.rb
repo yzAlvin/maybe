d4dbaa5328898aa0c95254e709f753e46496960e
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2025-08-02 19:33:09 +1000

    Remove Plaid in tests
    Still a lot of other Plaid code
    
    But now all tests pass locally..

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -38,9 +38,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     merchant = merchants(:netflix)
     import = imports(:transaction)
     budget = budgets(:one)
-    plaid_item = plaid_items(:one)
-
-    Provider::Plaid.any_instance.expects(:remove_item).with(plaid_item.access_token).once
 
     perform_enqueued_jobs(only: FamilyResetJob) do
       delete reset_user_url(@user)
@@ -55,7 +52,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not Merchant.exists?(merchant.id)
     assert_not Import.exists?(import.id)
     assert_not Budget.exists?(budget.id)
-    assert_not PlaidItem.exists?(plaid_item.id)
   end
 
   test "non-admin cannot reset family data" do
